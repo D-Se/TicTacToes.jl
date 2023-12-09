3ca5851e13358798f18133abc980cc79a917c931
D-Se <donaldseinen@proton.me>
2023-12-09 16:02:57 +0100

    add show tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,21 @@ using Test
 macro alloc(expr)
     argnames = [gensym() for a in expr.args]
     quote
-        function g($(argnames...))
-            @allocated $(Expr(expr.head, argnames...))
-        end
+        g($(argnames...)) = @allocated $(Expr(expr.head, argnames...))
         $(Expr(:call, :g, (esc(a) for a in expr.args)...))
     end
 end
 
 @testset "TicTacToes.jl" begin
     g = TicTacToe()
+    g2 = TicTacToe(0x0044, 0x0090, true, 5, RUN)
     @test_throws "Position" play!(g, -1)
     @test 0            == @alloc play!(g, 1)
+    @test "X--\n---\n---\n" == repr(g)
+    @test "Draw!"      == repr(TicTacToe(0x0000, 0x0000, true, 0, DRAW))
     @test g.x          == 0x004
     @test 0            == @alloc ai!(g)
     @test (0, 1, 2, 3) == Int.(instances(GameState))
+    ai!(g2)
+    @test "X has won!" == repr(g2)
 end
